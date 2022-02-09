622a81ff1419cbbf37f54591c3671549892b1f53
Alec Johnson <alecjohnsonhome@gmail.com>
2022-02-09 22:42:56 +0000

    Tiny amount of progress

--- tests/mock_spotipy_client.py
+++ tests/mock_spotipy_client.py
@@ -1,82 +1,92 @@
-playlist_data = {
+playlists_response = {
     "href": "https://api.spotify.com/v1/users/mock_user/playlists?offset=0&limit=50",
     "items": [
         {
-            "collaborative": False,
-            "description": "Test playlist",
-            "external_urls": {"spotify": "https://open.spotify.com/playlist/1"},
-            "href": "https://api.spotify.com/v1/playlists/1",
             "id": "test_playlist_1",
-            "images": [
-                {
-                    "height": 640,
-                    "url": "https://i.scdn.co/image/1",
-                    "width": 640,
-                }
-            ],
             "name": "Test playlist 1",
-            "owner": {
-                "display_name": "mock_user",
-                "external_urls": {"spotify": "https://open.spotify.com/user/mock_user"},
-                "href": "https://api.spotify.com/v1/users/mock_user",
-                "id": "mock_user",
-                "type": "user",
-                "uri": "spotify:user:mock_user",
-            },
-            "primary_color": None,
-            "public": False,
-            "snapshot_id": "snapshot_string==",
-            "tracks": {
-                "href": "https://api.spotify.com/v1/playlists/1/tracks",
-                "total": 9,
-            },
-            "type": "playlist",
-            "uri": "spotify:playlist:1",
         },
         {
-            "collaborative": False,
-            "description": "Test playlist 2",
-            "external_urls": {"spotify": "https://open.spotify.com/playlist/2"},
-            "href": "https://api.spotify.com/v1/playlists/2",
             "id": "test_playlist_2",
-            "images": [
-                {
-                    "height": 640,
-                    "url": "https://i.scdn.co/image/2",
-                    "width": 640,
-                }
-            ],
             "name": "Test playlist 2",
-            "owner": {
-                "display_name": "mock_user",
-                "external_urls": {"spotify": "https://open.spotify.com/user/mock_user"},
-                "href": "https://api.spotify.com/v1/users/mock_user",
-                "id": "mock_user",
-                "type": "user",
-                "uri": "spotify:user:mock_user",
-            },
-            "primary_color": None,
-            "public": False,
-            "snapshot_id": "snapshot_string==",
-            "tracks": {
-                "href": "https://api.spotify.com/v1/playlists/2/tracks",
-                "total": 90,
-            },
-            "type": "playlist",
-            "uri": "spotify:playlist:2",
         },
     ],
-    "limit": 50,
-    "next": None,
-    "offset": 0,
-    "previous": None,
     "total": 2,
+}
+playlist_response = {
+    "tracks": {
+        "items": [
+            {
+                "track": {
+                    "album": {
+                        "artists": [
+                            {
+                                "name": "Test artist",
+                            }
+                        ],
+                        "id": "album_id_1",
+                        "images": [
+                            {
+                                "height": 640,
+                                "url": "https://i.scdn.co/image/album_1_big",
+                                "width": 640,
+                            },
+                            {
+                                "height": 300,
+                                "url": "https://i.scdn.co/image/album_1_medium",
+                                "width": 300,
+                            },
+                            {
+                                "height": 64,
+                                "url": "https://i.scdn.co/image/album_1_small",
+                                "width": 64,
+                            },
+                        ],
+                        "name": "Test Album One",
+                    }
+                }
+            },
+            {
+                "track": {
+                    "album": {
+                        "artists": [
+                            {
+                                "name": "Test artist 2",
+                            }
+                        ],
+                        "id": "album_id_2",
+                        "images": [
+                            {
+                                "height": 640,
+                                "url": "https://i.scdn.co/image/album_2_big",
+                                "width": 640,
+                            },
+                            {
+                                "height": 300,
+                                "url": "https://i.scdn.co/image/album_2_medium",
+                                "width": 300,
+                            },
+                            {
+                                "height": 64,
+                                "url": "https://i.scdn.co/image/album_2_small",
+                                "width": 64,
+                            },
+                        ],
+                        "name": "Test Album Two",
+                    }
+                }
+            },
+        ]
+    }
 }
 
 
 class MockSpotipy:
     def __init__(self):
-        self.playlists = playlist_data
+        self.playlists_response = playlists_response
+        self.playlist_response = playlist_response
 
     def current_user_playlists(self):
-        return self.playlists
+        return self.playlists_response
+
+    def playlist(self, playlist_id, fields):
+        return self.playlist_response
