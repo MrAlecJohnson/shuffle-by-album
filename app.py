d852086f68a79c3e8b4cf285af8d29c89ba52264
Alec Johnson <alecjohnsonhome@gmail.com>
2022-02-14 20:21:12 +0000

    Moved much of layout into functions

--- app.py
+++ app.py
@@ -2,18 +2,17 @@ import spotipy
 import streamlit as st
 
 from shuffle_by_album.spotify_functions import (
-    NotEnoughAlbums,
     authenticate_spotify,
     get_params,
     playlist_albums,
-    pick_albums,
-    extract_album_info,
 )
 from shuffle_by_album.streamlit_functions import (
     cache_playlists,
-    display_album,
+    input_playlist_picker,
+    album_count_slider,
+    output_playlist_picker,
+    album_picker_button,
     submit_button,
-    reset_album_choices,
 )
 from shuffle_by_album.constants import title, params_filename
 
@@ -38,49 +37,20 @@ def main():
     playlists = cache_playlists(sp)
 
     # Sidebar
-    input_playlist_name = st.sidebar.selectbox(
-        "Which playlist do you want albums from?",
-        playlists.keys(),
-        on_change=reset_album_choices,
-    )
+    input_playlist_name = input_playlist_picker(playlists)
     if input_playlist_name:
         st.session_state.input_playlist_id = playlists[input_playlist_name]
+        potential_albums = playlist_albums(sp, st.session_state.input_playlist_id)
 
-    st.session_state.album_count = st.sidebar.slider(
-        "How many albums would you like to pick?",
-        1,
-        10,
-        value=1,
-        on_change=reset_album_choices,
-    )
+    st.session_state.album_count = album_count_slider()
 
-    output_playlist_name = st.sidebar.selectbox(
-        "Which playlist do you want to add the albums to?", playlists.keys()
-    )
+    output_playlist_name = output_playlist_picker(playlists)
     if output_playlist_name:
         st.session_state.output_playlist_id = playlists[output_playlist_name]
 
-    potential_albums = playlist_albums(sp, st.session_state.input_playlist_id)
-
     #  Main panel
     st.title(title)
-
-    if st.button("Pick albums"):
-        try:
-            st.session_state.albums = pick_albums(
-                potential_albums, st.session_state.album_count
-            )
-            st.session_state.album_info = extract_album_info(
-                sp, st.session_state.albums
-            )
-            "Albums chosen"
-            for a in st.session_state.album_info:
-                display_album(a)
-
-        except NotEnoughAlbums as e:
-            st.session_state.albums = []
-            st.write(str(e))
-
+    album_picker_button(sp, potential_albums)
     submit_button(sp)
 
 
