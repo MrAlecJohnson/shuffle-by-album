2b1065a8b32ae3052fd050b14fe4107db5082ef7
Alec Johnson <alecjohnsonhome@gmail.com>
2022-02-18 19:19:28 +0000

    Added authentication to minimal app

--- minimal.py
+++ minimal.py
@@ -1,8 +1,24 @@
+from os import getenv
+
+from dotenv import load_dotenv
+import spotipy
 import streamlit as st
+
+from shuffle_by_album.spotify_functions import (
+    authenticate_spotify,
+)
+from shuffle_by_album.constants import redirect_uri
 
 
 def main():
     st.title("This is a title")
+    load_dotenv()
+    client_id = getenv("CLIENT_ID")
+    client_secret = getenv("CLIENT_SECRET")
+
+    auth = authenticate_spotify(client_id, client_secret, redirect_uri)
+    sp = spotipy.Spotify(auth_manager=auth)
+    print(sp)
 
 
 if __name__ == "__main__":
